12d7a7f8311d991895546fb4fdbeea89f9a30571
iferc <github@iferc.ca>
2021-12-03 15:18:57 -0500

    Refactor a bit of cleanup.

--- day03/src/first/mod.rs
+++ day03/src/first/mod.rs
@@ -3,8 +3,6 @@ pub mod test;
 
 use eyre::{eyre, Result as EyreResult};
 
-pub struct Thing {}
-
 pub fn challenge(input: &str) -> EyreResult<usize> {
     let lines = input
         .lines()
@@ -13,22 +11,20 @@ pub fn challenge(input: &str) -> EyreResult<usize> {
                 .map(|ch| match ch {
                     '0' => Ok(0),
                     '1' => Ok(1),
-                    _ => Err(eyre!("omg, we've been betrayed by the input!!!")),
+                    _ => Err(eyre!("betrayed by invalid input")),
                 })
                 .collect::<Result<Vec<usize>, _>>()
         })
         .collect::<Result<Vec<_>, _>>()?;
 
-    let total_lines = lines.len();
-
-    if total_lines == 0 {
-        return Err(eyre!("No lines givens, dafuq bro?"));
+    if lines.len() == 0 {
+        return Err(eyre!("no input give"));
     }
 
     let line_length = lines[0].len();
     let mut sums: Vec<usize> = (0..line_length).map(|_| 0).collect::<Vec<_>>();
 
-    for line in lines {
+    for line in &lines {
         for (index, value) in line.iter().enumerate() {
             sums[index] += value;
         }
@@ -36,11 +32,8 @@ pub fn challenge(input: &str) -> EyreResult<usize> {
 
     let mut gamma_bits = (0..line_length).map(|_| false).collect::<Vec<_>>();
     for (index, sum) in sums.iter().enumerate() {
-        gamma_bits[index] = sum * 2 > total_lines;
+        gamma_bits[index] = sum * 2 > lines.len();
     }
-
-    println!("sums: {:#?}", sums);
-    println!("gamma_bits: {:#?}", gamma_bits);
 
     let mut gamma = 0;
     let mut epsilon = 0;
@@ -48,8 +41,6 @@ pub fn challenge(input: &str) -> EyreResult<usize> {
         gamma += 2usize.pow(index as u32) * if *gamma_bit { 1 } else { 0 };
         epsilon += 2usize.pow(index as u32) * if *gamma_bit { 0 } else { 1 };
     }
-    println!("gamma: {:#?}", gamma);
-    println!("epsilon: {:#?}", epsilon);
 
     Ok(gamma * epsilon)
 }
