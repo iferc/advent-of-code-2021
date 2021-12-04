825401eacb46ff96235ec0381178a7eeef5a320f
iferc <github@iferc.ca>
2021-12-04 12:34:43 -0500

    Improve pivot test coverage.

--- day03/src/pivot_iter.rs
+++ day03/src/pivot_iter.rs
@@ -29,7 +29,7 @@ mod tests {
     use super::*;
 
     #[test]
-    pub fn example1() {
+    pub fn swaps_columns_and_rows() {
         let given = vec![
             // stop fmt
             vec![1, 2, 3, 4],
@@ -47,7 +47,7 @@ mod tests {
     }
 
     #[test]
-    pub fn example2() {
+    pub fn empty_iters_to_empty_vec() {
         let given: Vec<Vec<i32>> = vec![vec![]];
         let expected: Vec<Vec<i32>> = vec![];
 
@@ -55,25 +55,57 @@ mod tests {
     }
 
     #[test]
-    pub fn example3() {
-        let given: Vec<Vec<i32>> = vec![vec![1]];
-        let expected: Vec<Vec<i32>> = vec![vec![1]];
+    pub fn no_perceived_effect_on_single_element() {
+        let given = vec![vec![1]];
+        let expected = vec![vec![1]];
 
         assert_eq!(expected, collect_pivoted(given));
     }
 
     #[test]
-    pub fn example4() {
-        let given: Vec<Vec<i32>> = vec![vec![1, 2]];
-        let expected: Vec<Vec<i32>> = vec![vec![1], vec![2]];
+    pub fn single_child_vec() {
+        let given = vec![vec![1, 2]];
+        let expected = vec![vec![1], vec![2]];
 
         assert_eq!(expected, collect_pivoted(given));
     }
 
     #[test]
-    pub fn example5() {
-        let given: Vec<Vec<i32>> = vec![vec![1], vec![2]];
-        let expected: Vec<Vec<i32>> = vec![vec![1, 2]];
+    pub fn multiple_child_vecs() {
+        let given = vec![vec![1], vec![2]];
+        let expected = vec![vec![1, 2]];
+
+        assert_eq!(expected, collect_pivoted(given));
+    }
+
+    #[test]
+    pub fn truncates_to_first_none_in_row() {
+        let given = vec![
+            // stop fmt
+            vec![1, 2, 3, 4],
+            vec![1, 2],
+        ];
+        let expected = vec![
+            // stop fmt
+            vec![1, 1],
+            vec![2, 2],
+        ];
+
+        assert_eq!(expected, collect_pivoted(given));
+    }
+
+    #[test]
+    pub fn truncates_to_first_none_in_row_again() {
+        let given = vec![
+            // stop fmt
+            vec![1, 2],
+            vec![1, 2, 3, 4],
+        ];
+        let expected = vec![
+            // stop fmt
+            vec![1, 1],
+            vec![2, 2],
+        ];
 
         assert_eq!(expected, collect_pivoted(given));
     }
