ae0fd6df2bd4ffcee768aef05dffbcfa65ebf496
iferc <github@iferc.ca>
2021-12-05 15:14:05 -0500

    Add day 6 scaffolding.

---
+++ day06/benches/challenges.rs
@@ -0,0 +1,21 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use day06::{first, second};
+
+fn first_challenge(c: &mut Criterion) {
+    let input = include_str!("../data/challenge.txt");
+
+    c.bench_function("day 06 - challenge 1", |b| {
+        b.iter(|| first::challenge(black_box(input)))
+    });
+}
+
+fn second_challenge(c: &mut Criterion) {
+    let input = include_str!("../data/challenge.txt");
+
+    c.bench_function("day 06 - challenge 2", |b| {
+        b.iter(|| second::challenge(black_box(input)))
+    });
+}
+
+criterion_group!(benches, first_challenge, second_challenge);
+criterion_main!(benches);
